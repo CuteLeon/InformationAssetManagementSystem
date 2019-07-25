7cfb7f8b383947acae653baa13d9b339616a3ce3
Leon <Leon.ID@QQ.COM>
2019-07-25 23:25:37 +0800

    反射模型属性自动填充表格控件列头；

--- IAMS.Client/Controls/ModelContainer.Designer.cs
+++ IAMS.Client/Controls/ModelContainer.Designer.cs
@@ -31,6 +31,7 @@ namespace IAMS.Client.Controls
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.MainLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
             this.AddButton = new System.Windows.Forms.Button();
             this.SearchTextBox = new System.Windows.Forms.TextBox();
@@ -206,8 +207,18 @@ namespace IAMS.Client.Controls
             // 
             // MainDataGridView
             // 
+            this.MainDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
             this.MainDataGridView.BackgroundColor = System.Drawing.Color.White;
             this.MainDataGridView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.MainDataGridView.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.MainDataGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.MainDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.MainLayoutPanel.SetColumnSpan(this.MainDataGridView, 8);
             this.MainDataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
--- IAMS.Client/Controls/ModelContainer.cs
+++ IAMS.Client/Controls/ModelContainer.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows.Forms;
 using IAMS.Model;
 
 namespace IAMS.Client.Controls
@@ -11,9 +14,62 @@ namespace IAMS.Client.Controls
     {
         protected Type ModelType = typeof(TModel);
 
+        protected BindingSource ModelBindingSource = new BindingSource() { DataSource = typeof(TModel) };
+
         public ModelContainer()
         {
             this.InitializeComponent();
+
+            if (this.DesignMode)
+            {
+                return;
+            }
+
+            this.InitGridView();
+        }
+
+        private void InitGridView()
+        {
+            this.MainDataGridView.AutoGenerateColumns = false;
+            this.MainDataGridView.Columns.Clear();
+
+            (this.MainDataGridView as ISupportInitialize)?.BeginInit();
+            (this.ModelBindingSource as ISupportInitialize)?.BeginInit();
+            this.SuspendLayout();
+
+            foreach (var propery in this.ModelType.GetProperties()
+                .Where(prop => prop.CanRead))
+            {
+                try
+                {
+                    var column = new DataGridViewTextBoxColumn()
+                    {
+                        DataPropertyName = propery.Name,
+                        Name = $"{propery.Name}DataGridViewColumn",
+                    };
+
+                    var attrs = propery.GetCustomAttributes(typeof(DisplayNameAttribute), true);
+                    var first = attrs?.FirstOrDefault();
+                    var text = (first as DisplayNameAttribute)?.DisplayName ?? propery.Name;
+
+                    column.HeaderText = text;
+
+                    this.MainDataGridView.Columns.Add(column);
+                }
+                catch
+                {
+                }
+            }
+
+            this.MainDataGridView.DataSource = this.ModelBindingSource;
+
+            (this.MainDataGridView as ISupportInitialize)?.EndInit();
+            (this.ModelBindingSource as ISupportInitialize)?.EndInit();
+            this.ResumeLayout(false);
+        }
+
+        protected virtual void InitGridViewColumns(DataGridView dataGridView)
+        {
         }
 
         private void SearchButton_Click(object sender, System.EventArgs e)
